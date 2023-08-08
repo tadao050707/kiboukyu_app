6989dff0596181e1a031fdd9970a10a07f639583
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-08 18:14:21 +0900

    [clean]変数名変更
    ログイン後遷移先のuser情報の変数名をlogged_in_userに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    user_path(resource.id)
+  def after_sign_in_path_for(logged_in_user)
+    user_path(logged_in_user.id)
   end
   
   protected
