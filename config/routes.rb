2225ac4901bf6d7bf098d0afe4375b7f932ae435
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-08 17:48:40 +0900

    [add]userコントローラ作成
    マイページを作成の為、showのルーティング設定と各種ファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :users, only: [:show]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
