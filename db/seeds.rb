5673766f25a75be9804354de7347f5f8201be991
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-08 17:27:42 +0900

    [add]試験用ユーザー作成
    動作確認用のユーザーをseedデータで作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: "adminmin",
+  email: "admin@email.com",
+  password: "adminadmin",
+  admin: true
+)
+
+5.times do |n|
+  User.create!(
+      name: "user#{n+10}",
+      email: "#{n+10}@email.com",
+      password: "#{n+10}aaaaaa",
+      admin: false
+  )
+end
