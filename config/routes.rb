13b09249cdd48cbba7da3640e715b48249e5a787
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-12 15:31:48 +0900

    [add]group基礎ファイル作成
    groupモデルmigrationファイル・コントローラ・viewファイルの初期状態保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'group/new'
+  get 'group/create'
+  get 'group/show'
+  get 'group/edit'
+  get 'group/update'
+  get 'group/destroy'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :users, only: [:show]
