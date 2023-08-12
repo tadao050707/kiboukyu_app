2b0e02f74049d4bce644c1ee9b1cf08f7d8c5d5a
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-12 16:13:06 +0900

    [add]groupルーティング設定
    groupsのindex以外のルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'group/new'
-  get 'group/create'
-  get 'group/show'
-  get 'group/edit'
-  get 'group/update'
-  get 'group/destroy'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :users, only: [:show]
+
+  resources :groups, only: %i[new create show edit update destroy]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
