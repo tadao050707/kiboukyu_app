12a17ca61407f59ecf349fdc483b55587ae14589
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-13 13:35:27 +0900

    [add]userのshowアクション追記
    usersコントローラにグループ機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @group = groups_name(@user)
+  end
+
+  private
+  def groups_name(user)
+    if Grouping.where(user_id: user.id).present?
+      Group.select(:id, :name)
+    else
+      nil
+    end
   end
 end
