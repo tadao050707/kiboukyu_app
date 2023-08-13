003134cd2ac9a8067121c3c7b3b2433dc5406dfe
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-13 17:25:10 +0900

    [add]new・cerateアクション作成
    groupのnew・cerateアクションを定義

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,8 +1,18 @@
 class GroupController < ApplicationController
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    @group.owner = current_user
+    if @group.save
+      @group.invite_member(@group.owner)
+      redirect_to @group, notice: "#{@group.name}グループに参加しました。"
+    else
+      flash.now[:error] = "グループ作成に失敗しました"
+      render :new
+    end
   end
 
   def show
@@ -15,5 +25,10 @@ class GroupController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :owner_id)
   end
 end
