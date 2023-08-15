ce58b9292a652707fb3a2c04a03976d3be7d8c3e
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-15 14:05:22 +0900

    [add]groupingsコントローラ作成
    各アクションと必要メソッドの仮作成

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,10 +1,18 @@
 class GroupingsController < ApplicationController
   def create
+    grouping = self.user.groupings.create(group_id: params[:group_id])
+    redirect_to group_path(grouping.group_id), notice: "#{grouping.user.name}さんが参加されました。"
   end
 
   def update
+    grouping = self.user.groupings.find_by(id: params[:id]).update(leave_group: true)
+    redirect_to group_path(grouping.group_id), notice: "#{grouping.user.name}さんが除名されました。"
   end
 
   def destroy
+    user_name = self.user.name
+    back_url = grouping.group_id
+    grouping = self.user.groupings.find_by(id: params[:id]).destroy
+    redirect_to group_path(back_url), notice: "#{user_name}さんが完全に除名されました。"
   end
 end
