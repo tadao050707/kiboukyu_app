1d721835c666471753de25633a8517749bc75fcc
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-16 16:51:47 +0900

    [disable]binding.pryを外す
    一応書いとく

--- app/models/grouping_callback.rb
+++ app/models/grouping_callback.rb
@@ -1,9 +1,8 @@
 class GroupingCallback
   def before_update(grouping)
     target_grouping = Grouping.find(grouping.id)
-    binding.pry
     if target_grouping.group.owner == target_grouping.user
-      throw(:abort)
+      throw(:abort) 
     end
   end
 end
