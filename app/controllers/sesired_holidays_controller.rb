45bd6d93e044b434ac30bc4122db6ba6f93c6b46
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-18 13:59:46 +0900

    [fix]アクセス制限が上手く効かないエラー解消
    ボタンにパラメータの追加、privateメソッドの処理修正

--- app/controllers/sesired_holidays_controller.rb
+++ app/controllers/sesired_holidays_controller.rb
@@ -29,10 +29,8 @@ class SesiredHolidaysController < ApplicationController
   end
 
   def sesired_holiday_current_user_or_admin
-    @user = User.find(params[:id]) if params[:id].present?
     @user = User.find(params[:user_id]) if params[:user_id].present?
     @user = User.find(params[:sesired_holiday][:user_id]) if params[:sesired_holiday].present?
-    binding.pry
     redirect_to user_path(current_user), notice: "他のユーザーの希望休は申請出来ません" unless current_user == @user || current_user.admin
   end
 end
