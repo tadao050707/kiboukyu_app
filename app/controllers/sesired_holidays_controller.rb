9201e2b8d0c2a38637aaf37d5e1c1d6ae3353a6e
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-08-18 13:57:55 +0900

    [add]希望休取り下げボタン確認
    destroyアクション仮と動作確認仕様で作成

--- app/controllers/sesired_holidays_controller.rb
+++ app/controllers/sesired_holidays_controller.rb
@@ -1,5 +1,6 @@
 class SesiredHolidaysController < ApplicationController
   before_action :sesired_holiday_current_user_or_admin, only: %i[new create destroy]
+  
   def new
     @sesired_holiday = SesiredHoliday.new
     @group = Group.find(params[:group_id])
@@ -17,6 +18,9 @@ class SesiredHolidaysController < ApplicationController
   end
 
   def destroy
+    @sesired_holiday = SesiredHoliday.find(params[:id])
+    flash[:notice] = "#{@sesired_holiday.my_holiday}の希望休が取り消されました"
+    redirect_to grouping_path(params[:grouping_id])
   end
 
   private
@@ -25,8 +29,10 @@ class SesiredHolidaysController < ApplicationController
   end
 
   def sesired_holiday_current_user_or_admin
+    @user = User.find(params[:id]) if params[:id].present?
     @user = User.find(params[:user_id]) if params[:user_id].present?
     @user = User.find(params[:sesired_holiday][:user_id]) if params[:sesired_holiday].present?
+    binding.pry
     redirect_to user_path(current_user), notice: "他のユーザーの希望休は申請出来ません" unless current_user == @user || current_user.admin
   end
 end
